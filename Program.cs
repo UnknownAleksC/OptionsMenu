db2b8870555fd6c2540dddcd673c29d6246422d8
UnknownAleksC <aleksanderca@getacademy.no>
2024-03-07 10:32:06 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OptionsMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
